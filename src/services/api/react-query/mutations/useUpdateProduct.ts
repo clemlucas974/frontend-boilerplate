e366df387e8853151fe07c9311d1097cd17ea0fa
Clément Lucas <clement.lucas@quitoque.fr>
2022-01-24 13:56:21 +0100

    feat(services | api): create udpate product mutation

---
+++ src/services/api/react-query/mutations/useUpdateProduct.ts
@@ -0,0 +1,16 @@
+import { useMutation } from "react-query";
+import { AxiosResponse } from "axios";
+
+import { ApiService, UpdateProductType } from "src/services/api/ApiService";
+import { ApiError, UpdateProductReturn } from "src/types";
+
+export const useUpdateProduct = () => {
+  const apiService = ApiService.createInstance();
+
+  return useMutation<UpdateProductReturn, ApiError, UpdateProductType>(
+    async (params) => {
+      const response: AxiosResponse = await apiService.updateProduct(params);
+      return response.data;
+    }
+  );
+};
